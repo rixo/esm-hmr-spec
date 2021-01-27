e1568db7487af9ed96fa9e39117e3d2d26a833da
rixo <rixo@rixo.fr>
2021-01-28 00:21:35 +0100

    specify css lifecycle

--- test/css.spec.js
+++ test/css.spec.js
@@ -52,12 +52,10 @@ test('last version of CSS is not lost on importer update', dev(), async t => {
           }
         `,
       }),
-      await t.hmr.update(),
+      await t.hmr.idle(),
     ])
 
     loaded.wasNotCalled()
-
-    await new Promise(resolve => setTimeout(resolve, 150))
 
     const style = await getStyle()
     t.eq(style.color, 'rgb(0, 0, 255)', 'updated CSS is applied')
@@ -73,20 +71,17 @@ test('last version of CSS is not lost on importer update', dev(), async t => {
   // expected: still applies last version of CSS
   //
   {
-    await Promise.all([
+    await loaded.nextCallAfter(async () => {
       await t.fixture.write({
         'index.js': `
           import './index.css'
           loaded()
           import.meta.hot.accept()
         `,
-      }),
-      await t.hmr.update(),
-    ])
+      })
+    })
 
-    loaded.wasNotCalled()
-
-    await new Promise(resolve => setTimeout(resolve, 150))
+    loaded.wasCalled()
 
     const style = await getStyle()
     t.eq(
@@ -97,9 +92,7 @@ test('last version of CSS is not lost on importer update', dev(), async t => {
   }
 })
 
-// TODO should it? (probably, that's what you'd get in an app that _never_
-// imports it)
-test.skip('CSS is removed when no more importers', dev(), async t => {
+test('orphan CSS lifecycle', dev(), async t => {
   const loaded = t.spy()
 
   await t.page.exposeFunction('loaded', loaded)
@@ -133,9 +126,9 @@ test.skip('CSS is removed when no more importers', dev(), async t => {
     t.eq(style.color, 'rgb(255, 0, 0)')
   }
 
-  // --- 1: importer update
+  // --- 1: importer update ---
   //
-  // expected: still applies last version of CSS
+  // expected: the stylesheet is unapplied in the browser
   //
   {
     await loaded.nextCallAfter(async () => {
@@ -149,13 +142,111 @@ test.skip('CSS is removed when no more importers', dev(), async t => {
 
     loaded.wasCalled()
 
-    await new Promise(resolve => setTimeout(resolve, 150))
-
     const style = await getStyle()
     t.eq(
       style.color,
       'rgb(0, 0, 0)',
       'stylesheet is removed when it has no more active importers'
+    )
+  }
+
+  // --- 2: import the unmodified CSS back on the page ---
+  //
+  // expected: the CSS is reapplied immediately
+  //
+  {
+    await loaded.nextCallAfter(async () => {
+      await t.fixture.write({
+        'index.js': `
+            import './index.css'
+            loaded()
+            import.meta.hot.accept()
+          `,
+      })
+    })
+
+    loaded.wasCalled()
+
+    const style = await getStyle()
+    t.eq(
+      style.color,
+      'rgb(255, 0, 0)',
+      'removed stylesheet is added back when imported again'
+    )
+  }
+
+  // --- 3: remove the CSS again ---
+  //
+  // expected: the CSS is unapplied
+  //
+  {
+    await loaded.nextCallAfter(async () => {
+      await t.fixture.write({
+        'index.js': `
+          loaded()
+          import.meta.hot.accept()
+        `,
+      })
+    })
+
+    loaded.wasCalled()
+
+    const style = await getStyle()
+    t.eq(
+      style.color,
+      'rgb(0, 0, 0)',
+      'stylesheet is removed when it has no more active importers again'
+    )
+  }
+
+  // --- 4: update CSS while not imported ---
+  //
+  // expected: the CSS is not reapplied in the browser
+  //
+  {
+    await t.fixture.write({
+      'index.css': `
+        body {
+          color: rgb(0, 255, 0);
+        }
+      `,
+    })
+
+    // give some time for the incorrect update to happen
+    await new Promise(resolve => setTimeout(resolve, 50))
+
+    loaded.wasNotCalled()
+
+    const style = await getStyle()
+    t.eq(
+      style.color,
+      'rgb(0, 0, 0)',
+      'stylesheet is not reapplied if modified while not imported'
+    )
+  }
+
+  // --- 5: reimport CSS ---
+  //
+  // expected: modified stylesheet (while not imported) is now applied
+  //
+  {
+    await loaded.nextCallAfter(async () => {
+      await t.fixture.write({
+        'index.js': `
+          import './index.css'
+          loaded()
+          import.meta.hot.accept()
+        `,
+      })
+    })
+
+    loaded.wasCalled()
+
+    const style = await getStyle()
+    t.eq(
+      style.color,
+      'rgb(0, 255, 0)',
+      'stylesheet modified while not imported is applied when imported again'
     )
   }
 })
